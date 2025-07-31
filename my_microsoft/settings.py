eec5c8272d276c0e349b82b6bfadf8311ab64644
Anshika Chauhan <chauhananshika533@gmail.com>
2025-07-31 15:41:31 +0530

    collected staticfiles

--- my_microsoft/settings.py
+++ my_microsoft/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8%o6^2k_cshrnr8wi^lm@$61g5#a7$l7hezo@7vkmqz-!9&2w9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -119,6 +119,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
