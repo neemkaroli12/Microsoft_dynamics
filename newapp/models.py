468f74175125c07acbdafdc4104da7ca31c4b4d4
sakshikumari431 <baliramsingh67@gmail.com>
2025-07-31 15:07:36 +0530

    change commit

--- newapp/models.py
+++ newapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class DynamicsCourse(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    image = models.ImageField(upload_to='dynamics_courses/')
+    created_at = models.DateTimeField(auto_now_add=True)  # <-- Added
+
+    def __str__(self):
+        return self.title
+
--- newapp/views.py
+++ newapp/views.py
@@ -3,5 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.shortcuts import render, get_object_or_404, redirect
+from .models import DynamicsCourse
 def home(request):
     return render(request,"index.html")
+
+
+
+def dynamics_courses(request):
+    courses = DynamicsCourse.objects.all()
+    return render(request, 'index.html', {'courses': courses})
