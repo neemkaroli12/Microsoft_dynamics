f9a83236d383fa3d87f4c20b104ba96b515b66b3
Anshika Chauhan <chauhananshika533@gmail.com>
2025-07-31 13:58:41 +0530

    initial commit

--- newapp/views.py
+++ newapp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.shortcuts import render, get_object_or_404, redirect
+def home(request):
+    return render(request,"index.html")
