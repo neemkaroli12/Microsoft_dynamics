f366167258ecce61a1376d35452eb6d114a75558
Anshika Chauhan <chauhananshika533@gmail.com>
2025-08-13 12:43:18 +0530

    django

--- newapp/views.py
+++ newapp/views.py
@@ -23,6 +23,7 @@ def course_detail(request, slug):
    
 def about(request):
     return render(request, "about.html")
+
 def instructor(request):
     success = False
 
@@ -73,9 +74,8 @@ def contact_view(request):
                 fail_silently=False
             )
 
-            messages.success(request, "✅ Your message has been sent successfully!")
-            form = ContactForm()  # Clear form
-    else:
+            messages.success(request, " Your message has been sent successfully!")
+            form = ContactForm()  
         form = ContactForm()
 
     return render(request, 'contact.html', {'form': form})
