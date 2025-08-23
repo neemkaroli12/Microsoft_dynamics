b6c6048844ff1b88ecc0da58ffc0f36b6fc56d00
Anshika Chauhan <chauhananshika533@gmail.com>
2025-08-23 09:12:56 +0530

    first commit

--- my_microsoft/settings.py
+++ my_microsoft/settings.py
@@ -76,10 +76,21 @@ WSGI_APPLICATION = 'my_microsoft.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'microsoft_dynamics_db',   # yahan apna db name
+        'USER': 'dynamics_user',           # yahan user name
+        'PASSWORD': 'dynamics#123',       # password (agar bhool gaye ho to naya set karenge)
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
